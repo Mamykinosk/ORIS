7e270d29acb0f2729dac1d0fcf911a5fa9a9ec52
Ostap <mamykinosk@gmail.com>
2025-12-13 23:33:58 +0300

    README.md added

--- FirstSemester/MiniHttpServer/obj/Debug/net9.0/MiniHttpServer.AssemblyInfo.cs
+++ FirstSemester/MiniHttpServer/obj/Debug/net9.0/MiniHttpServer.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MiniHttpServer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a3ab84d546fdbe90dd592c526cc12aff6981836b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("MiniHttpServer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MiniHttpServer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
